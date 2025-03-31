9d01d7e8eb0156aa706a750092baa2e6a6cc7b94
u05730dg <daniel.groves@student.manchester.ac.uk>
2025-03-31 13:01:04 +0100

    simplified a little

--- Day 5/day5.1.py
+++ Day 5/day5.1.py
@@ -6,20 +6,20 @@ with open("input.txt","r") as file:
     curr_map = []
     for line in file:
         if line.startswith("seeds"):
-            seeds = [int(i) for i in line.strip().split(" ", 1)[1].split(" ")]
+            seeds = list(map(int, line.strip().split(" ", 1)[1].split(" ")))
         elif line.endswith("map:\n"):
             if curr_map: maps.append(curr_map)
             curr_map = []
         elif line.strip():
-            curr_map.append([int(i) for i in line.strip().split(" ")])
+            curr_map.append([int(i) for i in line.split()])
     if curr_map: maps.append(curr_map)
 
 for seed in seeds:
     tracker = seed
     for map in maps:
-        for mapping in map:
-            if mapping[1] <= tracker <= mapping[1] + mapping[2]:
-                tracker = mapping[0] + (tracker - mapping[1])
+        for dest, src, length in map:
+            if src <= tracker <= src + length:
+                tracker = dest + (tracker - src)
                 break
     lowest = min(lowest, tracker)
 
